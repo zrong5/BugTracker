c744a11514c1eb5efb78ca2256355a0c040e7089
zrong5 <zrong5@uic.edu>
2020-01-02 01:02:35 -0600

    fixed login partial view and added identity controller (login, logout, register)

---
+++ BugTrackerV1.0/Controllers/IdentityController.cs
@@ -0,0 +1,97 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTrackerV1._0.Controllers
+{
+    public class IdentityController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public IdentityController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(username);
+                if (user != null)
+                {
+                    var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+                    if (signInResult.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(string username, string password, string role)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = username,
+                    Email = "userEmail@asp.net"
+                };
+                var result = await _userManager.CreateAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    var currentUser = await _userManager.FindByEmailAsync(user.Email);
+
+                    // create role if it doesn't exists
+                    var roleResult = await _roleManager.FindByNameAsync(role);
+                    if (roleResult == null)
+                    {
+                        var adminRole = new IdentityRole(role);
+                        await _roleManager.CreateAsync(adminRole);
+                        //await _roleManager.AddClaimAsync(adminRole, new Claim("Can add roles", "add.role"));
+                    }
+                    await _userManager.AddToRoleAsync(currentUser, role);
+
+                    var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+                    if (signInResult.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
